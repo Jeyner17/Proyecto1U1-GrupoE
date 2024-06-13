bc1fd67672fdf18f689bbcaa9a17b0c1da29aee9
Mateo Beltrán <105222194+MateoBel20@users.noreply.github.com>
2024-06-13 02:14:16 -0500

    Parametrización V3
    Login Fase 7

---
+++ src/components/Mateo_Beltran/main.js
@@ -0,0 +1,7 @@
+import { LeftInfo } from '../Mateo_Beltran/leftInfo.js';
+import { RightLogin } from '../Mateo_Beltran/rightLogin.js';
+import {SolucionCard} from '../Mateo_Beltran/cards.js';
+
+customElements.define('left-info', LeftInfo);
+customElements.define('right-login', RightLogin);
+customElements.define('solucion-card', SolucionCard);
--- src/components/main.js
+++
@@ -1,7 +0,0 @@
-import { LeftInfo } from '../components/leftInfo.js';
-import { RightLogin } from '../components/rightLogin.js';
-import {SolucionCard} from '../components/cards.js';
-
-customElements.define('left-info', LeftInfo);
-customElements.define('right-login', RightLogin);
-customElements.define('solucion-card', SolucionCard);
